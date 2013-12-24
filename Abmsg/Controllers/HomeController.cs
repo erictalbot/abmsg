4554b5a9a0d346dee43cb05c37b5167855987b95
Eric Talbot <Talbot.Eric@gmail.com>
2013-12-23 20:19:46 -0500

    GetNews.cshtml

--- Abmsg/Controllers/HomeController.cs
+++ Abmsg/Controllers/HomeController.cs
@@ -7,11 +7,20 @@ using System.Web.Mvc;
 using AbmsgModel;
 using AbmsgModel.Data;
 using abmsgFB;
+using Abmsg.Models.Views;
 
 namespace Abmsg.Controllers
 {
     public class HomeController : Controller
     {
+        protected IUow Uow { get; set; }
+
+
+        public HomeController(IUow uow)
+        {
+            Uow = uow;
+        }
+
         public ActionResult Index()
         {
             ViewBag.baseUrl = "http://www.abmsg.ca/api/news/";
@@ -32,6 +41,13 @@ namespace Abmsg.Controllers
         public ActionResult NewsEdit()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetNews(int id)
+        {
+            HomeViewModel model = new HomeViewModel(Uow.News.GetById(id));
+            return View(model);
         }
     }
 }
---
+++ Abmsg/Models/Views/HomeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AbmsgModel.Data;
+
+namespace Abmsg.Models.Views
+{
+    public class HomeViewModel
+    {
+
+        #region properties
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        #endregion
+
+        #region ctor
+
+
+        public HomeViewModel(News news)
+        {
+            Id = news.Id;
+            Title = news.Title;
+            Text = news.Text;
+            Date = news.Date;
+        }
+
+        #endregion
+
+
+
+
+    }
+
+}
